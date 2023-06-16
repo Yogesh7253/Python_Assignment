4eb130806fdaa8aebeb43ea5d3c5394dbce8a51f
Yogesh7253 <131522921+Yogesh7253@users.noreply.github.com>
2023-06-16 13:52:16 +0530

    Delete 1st_que_ii.py

--- 1st_que_ii.py
+++
@@ -1,4 +0,0 @@
-import json
-dict1 = {'Haryana':'Chandigarh','Maharashtra':'Mumbai','Karnataka':'Bangluru','Rajasthan':'Jaipur','Uttar Pradesh':'Lucknow','Gujarat':'Gandhinagar','Goa':'Panaji'}
-file = open(r"C:\Users\Hp\OneDrive\Desktop\Edyoda Practice\Edyoda Python Assignment 6\assignment_7_file.json","w")
-json.dump(dict1,file)
